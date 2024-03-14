33fb942b3f351964bee33bca6813d8ca67e3bc4e
Claudio Barsante <clbmribas@gmail.com>
2024-03-14 18:59:30 -0300

    feat: add react hook form and validation with zod

--- src/pages/auth/sign-in.tsx
+++ src/pages/auth/sign-in.tsx
@@ -2,8 +2,26 @@ import { Button } from '@/components/ui/button';
 import { Input } from '@/components/ui/input';
 import { Label } from '@/components/ui/label';
 import { Helmet } from 'react-helmet-async';
+import { useForm } from 'react-hook-form';
+import { z } from 'zod';
 
+const schema = z.object({
+    email: z.string().email()
+});
+
+type SignInForm = z.infer<typeof schema>;
 export function SignIn() {
+    const {
+        register,
+        handleSubmit,
+        formState: { isSubmitting }
+    } = useForm<SignInForm>();
+
+    async function handleSignIn(data: SignInForm) {
+        console.log(data);
+
+        await new Promise((resolve) => setTimeout(resolve, 2000));
+    }
     return (
         <>
             <Helmet title="Sign In" />
@@ -17,12 +35,23 @@ export function SignIn() {
                             Acompanhe suas vendas pelo painel do parceiro!
                         </p>
                     </div>
-                    <form className="space-y-4">
+                    <form
+                        onSubmit={handleSubmit(handleSignIn)}
+                        className="space-y-4"
+                    >
                         <div className="space-y-2">
                             <Label htmlFor="email">Seu e-mail</Label>
-                            <Input id="email" type="email" />
+                            <Input
+                                id="email"
+                                type="email"
+                                {...register('email')}
+                            />
                         </div>
-                        <Button className="w-full" type="submit">
+                        <Button
+                            disabled={isSubmitting}
+                            className="w-full"
+                            type="submit"
+                        >
                             Acessar painel
                         </Button>
                     </form>
