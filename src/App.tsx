1e2a49c078e817239b44b226f13c902dca05d043
Claudio Barsante <clbmribas@gmail.com>
2024-03-14 15:58:49 -0300

    feat: add SEO tool, react-helmet-async

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,15 @@
+import { Helmet, HelmetProvider } from 'react-helmet-async';
 import { RouterProvider } from 'react-router-dom';
 
 import './global.css';
 import { router } from './routes';
 
+// -- '%s' is replaced with the name of the page
 export function App() {
-    return <RouterProvider router={router} />;
+    return (
+        <HelmetProvider>
+            <Helmet titleTemplate="%s | Pizza Shop 🍕" />
+            <RouterProvider router={router} />
+        </HelmetProvider>
+    );
 }
