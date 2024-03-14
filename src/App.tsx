50fbbe7b0fe10f19f11cff2348ce32999968417b
Claudio Barsante <clbmribas@gmail.com>
2024-03-14 13:35:45 -0300

    setup: ESlint and Prettier

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,9 @@
 import { Button } from './components/ui/button';
 import './global.css';
 export function App() {
-	return (
-		<div className='flex justify-center items-center h-screen'>
-			<Button>Enviar</Button>
-		</div>
-	);
+  return (
+    <div className="flex h-screen items-center justify-center">
+      <Button>Envifar</Button>
+    </div>
+  );
 }
