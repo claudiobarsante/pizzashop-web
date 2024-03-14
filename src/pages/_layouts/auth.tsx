46bcf0d80a3c238fd5e38789dbe82dd9329fe996
Claudio Barsante <clbmribas@gmail.com>
2024-03-14 18:41:36 -0300

    feat: add label and input to <Sign-in/> and red theme to the app

--- src/pages/_layouts/auth.tsx
+++ src/pages/_layouts/auth.tsx
@@ -1,4 +1,6 @@
+import { Pizza } from 'lucide-react';
 import { Outlet } from 'react-router-dom';
+import { twMerge } from 'tailwind-merge';
 
 /*/
  We need to tell the root route where we want it to render 
@@ -6,9 +8,23 @@ import { Outlet } from 'react-router-dom';
  */
 export function AuthLayout() {
     return (
-        <div>
-            <h1>Autenticação</h1>
-            <div>
+        <div className="min-h-screen grid grid-cols-2">
+            <div
+                className={twMerge(
+                    'flex flex-col justify-between h-full border-d border-foreground/5 ',
+                    'bg-muted p-10 text-muted-foreground'
+                )}
+            >
+                <div className="flex items-center gap-3 text-lg text-foreground">
+                    <Pizza className="size-6" />
+                    <span className="font-semibold">Pizza Shop</span>
+                </div>
+                <footer className="text-sm">
+                    Painel do parceiro &copy; {new Date().getFullYear()}
+                </footer>
+            </div>
+
+            <div className="flex flex-col justify-center items-center">
                 <Outlet />
             </div>
         </div>
