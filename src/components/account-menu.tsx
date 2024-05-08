274b1ccd5852f79f88ea930428159ecc8bac0e24
Claudio Barsante <clbmribas@gmail.com>
2024-05-08 12:55:46 -0300

    feat: add modal to update name and description of the restaurant

--- src/components/account-menu.tsx
+++ src/components/account-menu.tsx
@@ -7,11 +7,13 @@ import {
     DropdownMenuItem
 } from './ui/dropdown-menu';
 import { Button } from './ui/button';
-import { Building, ChevronDown, LogOut } from 'lucide-react';
+import { Building, ChevronDown, LogOut, StoreIcon } from 'lucide-react';
 import { useQuery } from '@tanstack/react-query';
 import { getProfile } from '@/api/get-profile';
 import { getManagedRestaurant } from '@/api/get-managed-restaurant';
 import { Skeleton } from './ui/skeleton';
+import { Dialog, DialogTrigger } from './ui/dialog';
+import { StoreProfileDialog } from './store-profile-dialog';
 
 export function AccountMenu() {
     const { data: profile, isLoading: isLoadingProfile } = useQuery({
@@ -26,48 +28,53 @@ export function AccountMenu() {
         });
 
     return (
-        <DropdownMenu>
-            <DropdownMenuTrigger asChild>
-                <Button
-                    variant="outline"
-                    className="flex select-none items-center gap-2"
-                >
-                    {isLoadingManagedRestaurant ? (
-                        <div className="space-y-1.5">
-                            <Skeleton className="h-4 w-32" />
-                            <Skeleton className="h-3 w-24" />
-                        </div>
-                    ) : (
-                        managedRestaurant?.name
-                    )}
-                    <ChevronDown className="size-4" />
-                </Button>
-            </DropdownMenuTrigger>
-            <DropdownMenuContent align="end" className="w-56">
-                <DropdownMenuLabel className="flex flex-col">
-                    {isLoadingProfile ? (
-                        <Skeleton />
-                    ) : (
-                        <>
-                            {' '}
-                            <span>{profile?.name}</span>
-                            <span className="text-xs font-normal text-muted-foreground">
-                                {profile?.email}
-                            </span>
-                        </>
-                    )}
-                </DropdownMenuLabel>
-                <DropdownMenuSeparator />
-                <DropdownMenuItem>
-                    <Building className="mr-3 size-4" />
-                    <span>Perfil da loja</span>
-                </DropdownMenuItem>
-                <DropdownMenuItem className="text-rose-500 dark:text-rose-400">
-                    <LogOut className="mr-3 size-4" />
-                    <span>Sair</span>
-                </DropdownMenuItem>
-            </DropdownMenuContent>
-        </DropdownMenu>
+        <Dialog>
+            <DropdownMenu>
+                <DropdownMenuTrigger asChild>
+                    <Button
+                        variant="outline"
+                        className="flex select-none items-center gap-2"
+                    >
+                        {isLoadingManagedRestaurant ? (
+                            <div className="space-y-1.5">
+                                <Skeleton className="h-4 w-32" />
+                                <Skeleton className="h-3 w-24" />
+                            </div>
+                        ) : (
+                            managedRestaurant?.name
+                        )}
+                        <ChevronDown className="size-4" />
+                    </Button>
+                </DropdownMenuTrigger>
+                <DropdownMenuContent align="end" className="w-56">
+                    <DropdownMenuLabel className="flex flex-col">
+                        {isLoadingProfile ? (
+                            <Skeleton />
+                        ) : (
+                            <>
+                                {' '}
+                                <span>{profile?.name}</span>
+                                <span className="text-xs font-normal text-muted-foreground">
+                                    {profile?.email}
+                                </span>
+                            </>
+                        )}
+                    </DropdownMenuLabel>
+                    <DropdownMenuSeparator />
+                    <DialogTrigger asChild>
+                        <DropdownMenuItem>
+                            <Building className="mr-3 size-4" />
+                            <span>Perfil da loja</span>
+                        </DropdownMenuItem>
+                    </DialogTrigger>
+                    <DropdownMenuItem className="text-rose-500 dark:text-rose-400">
+                        <LogOut className="mr-3 size-4" />
+                        <span>Sair</span>
+                    </DropdownMenuItem>
+                </DropdownMenuContent>
+            </DropdownMenu>
+            <StoreProfileDialog />
+        </Dialog>
     );
 }
 /* as you can not style the <DropdownMenuTrigger/> and it's responsable
