e549398753ddf03c87d297acc602b27cc6396419
Claudio Barsante <clbmribas@gmail.com>
2024-05-08 12:22:55 -0300

    feat: add loading state to <AccountMenu/>, set axios to delay requests just for testing

--- src/lib/axios.ts
+++ src/lib/axios.ts
@@ -5,3 +5,12 @@ export const api = axios.create({
     baseURL: env.VITE_API_URL,
     withCredentials: true // -- to send the cookies received from the backend on the request
 });
+
+// -- before  all requests, delay the requests in 2 seconds
+if (env.VITE_ENABLE_API_DELAY) {
+    // -- config is the object that contains the configuration of the request
+    api.interceptors.request.use(async (config) => {
+        await new Promise((resolve) => setTimeout(resolve, 2000));
+        return config;
+    });
+}
