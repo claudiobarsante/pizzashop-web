e22c27a081bf8b3be20ac33b37421724b55352c7
Claudio Barsante <clbmribas@gmail.com>
2024-05-08 13:31:03 -0300

    feat: add update profile

--- src/components/account-menu.tsx
+++ src/components/account-menu.tsx
@@ -18,13 +18,15 @@ import { StoreProfileDialog } from './store-profile-dialog';
 export function AccountMenu() {
     const { data: profile, isLoading: isLoadingProfile } = useQuery({
         queryKey: ['profile'],
-        queryFn: getProfile
+        queryFn: getProfile,
+        staleTime: Infinity
     });
 
     const { data: managedRestaurant, isLoading: isLoadingManagedRestaurant } =
         useQuery({
             queryKey: ['managed-restaurant'],
-            queryFn: getManagedRestaurant
+            queryFn: getManagedRestaurant,
+            staleTime: Infinity //-- not to refetch when the window loads
         });
 
     return (
