a7835bbda99df54f85c99e8490f384da772603f6
Claudio Barsante <clbmribas@gmail.com>
2024-05-13 17:08:45 -0300

    feat: add skeleton to order details

--- src/pages/app/orders/order-details.tsx
+++ src/pages/app/orders/order-details.tsx
@@ -19,6 +19,7 @@ import {
     TableHeader,
     TableRow
 } from '@/components/ui/table';
+import { OrderDetailsSkeleton } from './order-details-skeleton';
 
 export interface OrderDetailsProps {
     orderId: string;
@@ -40,7 +41,7 @@ export function OrderDetails({ orderId, open }: OrderDetailsProps) {
                 <DialogTitle>Pedido: {orderId}</DialogTitle>
                 <DialogDescription>Detalhes do pedido</DialogDescription>
             </DialogHeader>
-            {order && (
+            {order ? (
                 <div className="space-y-6">
                     <Table>
                         <TableBody>
@@ -150,6 +151,8 @@ export function OrderDetails({ orderId, open }: OrderDetailsProps) {
                         </TableFooter>
                     </Table>
                 </div>
+            ) : (
+                <OrderDetailsSkeleton />
             )}
         </DialogContent>
     );
