a93d51b547445da58b7f2aff7c163612258e7caa
Claudio Barsante <clbmribas@gmail.com>
2024-05-13 14:13:57 -0300

    feat: add redirection to sign in page when the user is not authenticated

--- src/pages/_layouts/app.tsx
+++ src/pages/_layouts/app.tsx
@@ -1,10 +1,34 @@
-import { Outlet } from 'react-router-dom';
 import { Header } from '@/components/header';
-
+import { isAxiosError } from 'axios';
+import { useEffect } from 'react';
+import { Outlet, useNavigate } from 'react-router-dom';
+import { api } from '@/lib/axios';
 /*/
  We need to tell the root route where we want it to render its child routes. We do that with <Outlet>.
  */
 export function AppLayout() {
+    const navigate = useNavigate();
+    // -- We need to redirect the user to the sign-in page if they try to access a page that requires authentication but they are not logged in.
+    useEffect(() => {
+        const interceptorId = api.interceptors.response.use(
+            (response) => response, // -- if no errors, we return the response
+            (error) => {
+                if (isAxiosError(error)) {
+                    const status = error.response?.status;
+                    const code = error.response?.data.code;
+
+                    if (status === 401 && code === 'UNAUTHORIZED') {
+                        navigate('/sign-in', { replace: true }); // -- disable
+                    }
+                }
+            }
+        );
+
+        // We need to clean up the interceptor when the component unmounts.
+        return () => {
+            api.interceptors.response.eject(interceptorId);
+        };
+    }, [navigate]);
     return (
         <div className="flex min-h-screen flex-col antialiased">
             <Header />
